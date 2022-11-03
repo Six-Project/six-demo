b7adbc11c57b8c3945b64e0fa0ebb98fa5de4d71
TsangHans <gzzenghan@189.cn>
2022-11-04 02:29:40 +0800

    add "six demo"

---
+++ plugins/hello/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+"""
+
+@Auther: niu
+@FileName: __init__.py.py
+@Introduction: 
+
+"""
+import mini_six as six
+import cv2 as cv
+
+
+@six.watch("screenshot", 0x10010, period=100)
+def action(image):
+    cv.imshow("hello six", image)
+    cv.waitKey()
+    cv.destroyAllWindows()
