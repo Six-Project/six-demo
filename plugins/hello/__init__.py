3a7d82ed6a0e6475e56b674780d14b31bc34d00c
hans <gzzenghan@189.cn>
2022-11-05 19:51:23 +0800

    rectify "hello"

--- plugins/hello/__init__.py
+++ plugins/hello/__init__.py
@@ -10,7 +10,7 @@ import mini_six as six
 import cv2 as cv
 
 
-@six.watch("screenshot", 0x10010, period=100)
+@six.watch(six.DataSource.SCREENSHOT, 0x10010, period=100)
 def action(image):
     cv.imshow("hello six", image)
     cv.waitKey()
